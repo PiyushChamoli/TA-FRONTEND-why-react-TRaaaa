f53cbd2eb7c68762dc581b2dcd6c558917343562
Piyush Chamoli <piyushchamoli11@gmail.com>
2022-03-23 20:43:16 +0530

    add

---
+++ block-BRaaaa/code/script.js
@@ -0,0 +1,24 @@
+class TodoList {
+    constructor(list = []) {
+        this.todos = list
+    }
+
+    add(text) {
+        let todo = new Todo(text)
+        this.todos.push(todo)
+    }
+
+    delete(id) {
+        let index = this.todos.findIndex((todo) => todo.id === id)
+        this.todos.splice(index,1)
+        
+    }
+}
+
+class Todo {
+    constructor(text) {
+        this.text = text
+        this.isDone = false
+        this.id = `id-${Date.now()}`
+    }
+}
