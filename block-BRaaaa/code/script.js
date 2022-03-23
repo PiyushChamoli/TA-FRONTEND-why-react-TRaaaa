b0e30be43477299455a9297abe9de0ccd5b8b7e1
Piyush Chamoli <piyushchamoli11@gmail.com>
2022-03-24 01:20:58 +0530

    update script.js

--- block-BRaaaa/code/script.js
+++ block-BRaaaa/code/script.js
@@ -1,24 +1,50 @@
-class TodoList {
-    constructor(list = []) {
-        this.todos = list
-    }
+let input = document.querySelector(`input[type="text"]`);
+let rootElm = document.querySelector('.todos');
 
-    add(text) {
-        let todo = new Todo(text)
-        this.todos.push(todo)
+let allMovies = [
+    {
+        name:'Forest Gump',
+        watched: false
+    },
+    {
+        name:'Inception',
+        watched: false
     }
+]
 
-    delete(id) {
-        let index = this.todos.findIndex((todo) => todo.id === id)
-        this.todos.splice(index,1)
-        return this.todos.elgnth
+input.addEventListener('keyup', (event) => {
+    if (event.keyCode === 13) {
+        allMovies.push({
+            name: event.target.value,
+            watched:false
+        })
+        event.target.value = '';
+        createMovieUI(allMovies, rootElm)
     }
+})
+
+function handleChange(event) {
+    let id = event.target.id;
+    allMovies[id].watched = !allMovies[id].watched
+    createMovieUI(allMovies, rootElm)
 }
 
-class Todo {
-    constructor(text) {
-        this.text = text
-        this.isDone = false
-        this.id = `id-${Date.now()}`
-    }
+function createMovieUI(data, root) {
+    root.innerHTML = '';
+    data.forEach((movie,i) => {
+        let li = document.createElement('li');
+        let button = document.createElement('button');
+        button.id = i;
+        button.innerText = movie.watched ? 'Watched' : 'To Watch';
+        button.addEventListener('click', handleChange);
+        let label = document.createElement('label');
+        label.for = i;
+        label.innerText = movie.name;
+
+        li.append(label, button);
+
+        rootElm.append(li);
+    })
 }
+
+createMovieUI(allMovies, rootElm)
