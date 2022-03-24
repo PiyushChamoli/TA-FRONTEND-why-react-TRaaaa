54493c4ca0ad382d67b5b2a061aa2514ab0a757a
Piyush Chamoli <piyushchamoli11@gmail.com>
2022-03-24 22:10:53 +0530

    update script.js

--- block-BRaaac/code/script.js
+++ block-BRaaac/code/script.js
@@ -1,5 +1,5 @@
 let input = document.querySelector(`input[type="text"]`);
-let rootElm = document.querySelector('.todos');
+let rootElm = document.querySelector('.movies');
 
 let allMovies = [
     {
@@ -23,31 +23,46 @@ input.addEventListener('keyup', (event) => {
     }
 })
 
+function handleChange(event) {
+    let id = event.target.dataset.id;
+    console.log('id:',id)
+    allMovies[id].watched = !allMovies[id].watched
+    createMovieUI(allMovies, rootElm)
+}
+
 function elm(type, attr= {}, ...children) {
     let element = document.createElement(type);
     for (let key in attr) {
         if(key.startsWith('data-')) {
             element.setAttribute(key, attr[key])
+            console.log('key:',key,'attr:',attr[key])
+        } else if (key.startsWith('on')) {
+            let eventType = key.replace('on','').toLowerCase();
+            element.addEventListener(eventType, attr[key]);
         } else {
             element[key] = attr[key]
         }
     }
+    children.forEach((child) => {
+        if (typeof child === 'object') {
+            element.append(child)
+        } 
+        if (typeof child === 'string') {
+            let node = document.createTextNode(child)
+            element.append(node)
+        }
+    })
     return element;
-}
-
-function handleChange(event) {
-    let id = event.target.id;
-    allMovies[id].watched = !allMovies[id].watched
-    createMovieUI(allMovies, rootElm)
 }
 
 function createMovieUI(data, root) {
     root.innerHTML = '';
     data.forEach((movie,i) => {
         let li = elm(
-            ('li'), {}, 
-            elm(('button'), { "data-id":i, onClick:handleChange }, movie.watched),
-            elm(('label'), { for:i }, movie.name)    
+            'li', 
+            {}, 
+            elm('label', { for:i }, movie.name),    
+            elm('button', { "data-id":i, onClick:handleChange }, movie.watched ? "Watched" : "To Watch"),
         );
         
         rootElm.append(li);
